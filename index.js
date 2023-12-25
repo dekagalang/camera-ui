f1e0f9ea1b33c0359da42427f1ef7bc07fdea6de
deka galang <takikta1771@gmail.com>
2023-12-25 05:38:04 +0000

    progress

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
+// /**
+//  * @format
+//  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import 'react-native-gesture-handler';
 import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import { App } from './src/App';
 
 AppRegistry.registerComponent(appName, () => App);
+
