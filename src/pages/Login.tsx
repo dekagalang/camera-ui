ded2426981bdd29e7176e17c682931d265293348
deka galang <dekagalangpermadi@gmail.com>
2024-01-12 20:59:21 +0700

    progress

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -11,11 +11,26 @@ import {
   VStack,
 } from '@gluestack-ui/themed';
 import { useIsFocused } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {StatusBar, StyleSheet, View, SafeAreaView} from 'react-native';
+import { account, ID } from '../lib/appwrite';
 
 function Login({navigation}: any) {
+  const [loggedInUser, setLoggedInUser] = useState({name: ''});
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
   const isFocused = useIsFocused();
+
+  async function login(email:string, password:string) {
+    await account.createEmailSession(email, password);
+    setLoggedInUser(await account.get());
+  };
+
+  useEffect(() => {
+    console.log(email);
+  },[email])
+
   return (
     <SafeAreaView>
       {isFocused ? <StatusBar animated={true} backgroundColor="#6474ff" /> : null}
@@ -27,7 +42,8 @@ function Login({navigation}: any) {
               fontSize={20}
               lineHeight={20}
               color="#fff">
-              Welcome Back!
+              Welcome Back! {loggedInUser && loggedInUser.name != '' ? `Logged in as ${loggedInUser.name}` : 'Not logged in'}
+              {/* Welcome Back! */}
             </Heading>
             <Heading
               marginVertical={10}
@@ -43,7 +59,7 @@ function Login({navigation}: any) {
                 <FormControlLabelText color="#fff">Email</FormControlLabelText>
               </FormControlLabel>
               <Input variant="underlined">
-                <InputField color="#fff" />
+                <InputField color="#fff" onChange={(e) => setEmail(e.nativeEvent.text)} />
               </Input>
             </FormControl>
             <FormControl marginBottom={20}>
@@ -53,7 +69,7 @@ function Login({navigation}: any) {
                 </FormControlLabelText>
               </FormControlLabel>
               <Input variant="underlined">
-                <InputField color="#fff" type="password" />
+                <InputField color="#fff" type="password" onChange={(e) => setPassword(e.nativeEvent.text)} />
               </Input>
             </FormControl>
             <Button
@@ -62,7 +78,8 @@ function Login({navigation}: any) {
               borderRadius={100}
               marginTop={20}
               alignItems="center"
-              onPress={() => navigation.navigate('Home')}>
+              // onPress={() => navigation.navigate('Home')}>
+              onPress={() => login(email, password)}>
               <ButtonText color="#fff">Login</ButtonText>
             </Button>
             <Button
@@ -72,7 +89,12 @@ function Login({navigation}: any) {
               borderRadius={100}
               marginTop={20}
               alignItems="center"
-              onPress={() => navigation.navigate('Register')}>
+              // onPress={() => navigation.navigate('Register')}>
+              // onPress={() => login(email, password)}>
+              onPress={async () => {
+                await account.create(ID.unique(), email, password, name);
+                login(email, password);
+              }}>
               <ButtonText color="#fff">Register</ButtonText>
             </Button>
           </VStack>
